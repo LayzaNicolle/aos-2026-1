d17decc4f40ca57381f5baff959d2227c6261c86
Layza Nicolle <129307127+LayzaNicolle@users.noreply.github.com>
2026-03-27 01:31:23 +0000

    ajustes

--- express-01/api/models/index.js
+++ express-01/api/models/index.js
@@ -1,9 +1,11 @@
 import 'dotenv/config';
 import SequelizePkg from "sequelize"; // Importando todo o pacote
 const { Sequelize, DataTypes } = SequelizePkg;
+import pg from "pg"; // ✅ Import do pg via ES Module
 
 import getUserModel from "./user.js";
 import getMessageModel from "./message.js";
+
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
@@ -14,13 +16,14 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
       rejectUnauthorized: false,
     },
   },
-  dialectModule: require("pg"),
+  dialectModule: pg, // ✅ Aqui usamos o import, não require
 });
 
 const models = {
   User: getUserModel(sequelize, { DataTypes }),
   Message: getMessageModel(sequelize, { DataTypes }),
 };
+
 
 Object.values(models).forEach((model) => {
   if ("associate" in model) {
