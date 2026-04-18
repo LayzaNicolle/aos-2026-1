bdb33d7ffb6b18a6ac48f421ba58a4eb08dd7b45
Layza Nicolle <129307127+LayzaNicolle@users.noreply.github.com>
2026-04-18 23:46:51 +0000

    conectanto banco

--- tarefas-api/api/routes/tarefa.js
+++ tarefas-api/api/routes/tarefa.js
@@ -5,8 +5,5 @@ const router = Router();
 
 router.get('/', tarefaController.listarTarefas);
 router.post('/', tarefaController.criarTarefa);
-router.get('/:id', tarefaController.buscarTarefa);
-router.put('/:id', tarefaController.atualizarTarefa);
-router.delete('/:id', tarefaController.deletarTarefa);
 
 export default router;
