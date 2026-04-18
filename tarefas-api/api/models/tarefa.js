b36f3af1bc87a773d504c19eef492af98c0161d0
Layza Nicolle <129307127+LayzaNicolle@users.noreply.github.com>
2026-04-18 21:09:27 +0000

    CRUD

---
+++ tarefas-api/api/models/tarefa.js
@@ -0,0 +1,36 @@
+import { Sequelize, DataTypes } from 'sequelize';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  protocol: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+  dialectModule: require("pg"),
+});
+
+const Tarefa = sequelize.define('Tarefa', {
+    objectId: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true
+    },
+    descricao: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    concluida: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+    }
+});
+
+sequelize.sync();
+
+export default Tarefa;
