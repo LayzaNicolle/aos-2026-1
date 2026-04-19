995d008eee72b39d394e5130d12999ae3a739631
Layza Nicolle <129307127+LayzaNicolle@users.noreply.github.com>
2026-04-19 00:18:30 +0000

    ajuste

--- tarefas-api/api/routes/tarefa.js
+++ tarefas-api/api/routes/tarefa.js
@@ -5,5 +5,7 @@ const router = Router();
 
 router.get('/', tarefaController.listarTarefas);
 router.post('/', tarefaController.criarTarefa);
+router.put('/:id', tarefaController.atualizarTarefa);
+router.delete('/:id', tarefaController.deletarTarefa);
 
 export default router;
