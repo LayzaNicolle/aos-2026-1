cb7bc70423422a6efd7b38c89dbe98b5fa2759e5
Layza Nicolle <129307127+LayzaNicolle@users.noreply.github.com>
2026-04-19 03:15:29 +0000

    Ajustando rotas

--- tarefas-api/api/routes/tarefa.js
+++ tarefas-api/api/routes/tarefa.js
@@ -4,8 +4,9 @@ import tarefaController from '../controllers/tarefaController.js';
 const router = Router();
 
 router.get('/', tarefaController.listarTarefas);
+router.get('/:objectId', tarefaController.buscarTarefa);
 router.post('/', tarefaController.criarTarefa);
-router.put('/:id', tarefaController.atualizarTarefa);
-router.delete('/:id', tarefaController.deletarTarefa);
+router.put('/:objectId', tarefaController.atualizarTarefa);
+router.delete('/:objectId', tarefaController.deletarTarefa);
 
 export default router;
