936d2e7db460864e3c86b5cd89e650d430e2e0ba
Layza Nicolle <129307127+LayzaNicolle@users.noreply.github.com>
2026-05-04 20:12:23 +0000

    Ajustes no service

---
+++ curriculo-api/api/controllers/index.js
@@ -0,0 +1,11 @@
+import * as usuarioController from './usuarioController.js';
+import * as academicaController from './academicaController.js';
+import * as profissionalController from './profissionalController.js';
+import * as projetoController from './projetoController.js';
+
+export {
+  usuarioController,
+  academicaController,
+  profissionalController,
+  projetoController
+};
---
+++ curriculo-api/api/services/index.js
@@ -0,0 +1,11 @@
+import * as usuarioService from './usuarioService.js';
+import * as academicaService from './academicaService.js';
+import * as profissionalService from './profissionalService.js';
+import * as projetoService from './projetoService.js';
+
+export {
+  usuarioService,
+  academicaService,
+  profissionalService,
+  projetoService
+};
